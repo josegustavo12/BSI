6d6bb9cf34295729e624e309f4a760bc8cae48a4
José Gustavi <josegustavovictor@gmail.com>
2025-03-16 21:21:46 -0300

    POO e atv1 funcional

---
+++ 3° Semestre/Programação Funcional/atv1/atv1.hs
@@ -0,0 +1,19 @@
+-- escrever a formula de heron 
+
+main = do
+    a <- readLn
+    b <- readLn
+    c <- readLn
+    putStrLn $ heron a b c
+
+-- usando guarda 
+-- soma de dois lados tem que ser sempre maior que o outro lado
+heron a b c
+    | a + b < c = "-"
+    | a + c < b = "-"
+    | b + c < a = "-"
+    | otherwise  = show x
+    where
+        p = (a + b + c) / 2
+        x = sqrt (p * (p - a) * (p - b) * (p - c))
+
